44af5aeea7a69a6550e5865ce7cfe313fce89d02
Rory Coffey <coffeyrt@gmail.com>
2020-10-04 11:22:14 -0400

    added some info for raspberry pi crates pin and connections

--- Raspberry_Pi_Rust/forrest_hills_departure/src/main.rs
+++ Raspberry_Pi_Rust/forrest_hills_departure/src/main.rs
@@ -1,6 +1,7 @@
 #![no_std]
 
-extern crate sh1106;
+extern crate rppal;
+extern crate sh1106; // Crate for current SPI oled display // Crate for SPI, I2C, and GPIO on raspberry pi
 
 use sh1106::{mode::GraphicsMode, Builder};
 
