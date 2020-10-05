54b8f038da11a2ec04616acfe413999e30484f63
Rory Coffey <coffeyrt@gmail.com>
2020-10-05 08:49:23 -0400

    added dc and cs pin

--- Raspberry_Pi_Rust/forrest_hills_departure/src/main.rs
+++ Raspberry_Pi_Rust/forrest_hills_departure/src/main.rs
@@ -6,19 +6,23 @@ extern crate sh1106; // Crate for current SPI oled display
 
 use embedded_graphics::fonts::Font6x8;
 use embedded_graphics::prelude::*;
-use rppal::spi;
+use rppal::{gpio, spi};
 use sh1106::{mode::GraphicsMode, Builder};
 use std::{thread, time};
 
-
 fn main() {
-    let spi0 = spi::Spi::new(
-        bus = spi::Bus::Spi0,
-        slave_select = spi::SlaveSelect::Ss0,
-        clock_speed = 400_000,
-        mode = spi::Mode::Mode0,
-    );
-    let screen_display: GraphicsMode<_> = Builder::new().connect_spi(spi0).into();
+    let mut spi0 = spi::Spi::new(
+        spi::Bus::Spi0,
+        spi::SlaveSelect::Ss0,
+        400_000,
+        spi::Mode::Mode0,
+    )
+    .unwrap();
+    let spi_gpio = gpio::Gpio::new().unwrap();
+    let mut spi_dc = spi_gpio.get(24).unwrap().into_output();
+    let mut spi_cs = spi_gpio.get(8).unwrap().into_output();
+    let mut screen_display: GraphicsMode<_> =
+        Builder::new().connect_spi(spi0, spi_dc, spi_cs).into();
     screen_display.init().unwrap();
     screen_display.flush().unwrap();
     screen_display.draw(Font6x8::render_str("Hello world!", 1u8.into()).into_iter());
