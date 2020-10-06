ecaedc3fa07cff656abe17b7ab1c121aae34ba4f
Rory Coffey <coffeyrt@gmail.com>
2020-10-05 22:49:48 -0400

    directly used embedded graphics for text drawing. Cleared up most errors.  Still needs testing

--- Raspberry_Pi_Rust/forrest_hills_departure/src/main.rs
+++ Raspberry_Pi_Rust/forrest_hills_departure/src/main.rs
@@ -3,15 +3,19 @@
 extern crate embedded_graphics;
 extern crate rppal; // Crate for SPI, I2C, and GPIO on raspberry pi
 extern crate sh1106; // Crate for current SPI oled display
+extern crate std;
 
-use embedded_graphics::fonts::Font6x8;
 use embedded_graphics::prelude::*;
+use embedded_graphics::{
+    fonts::{Font6x8, Text},
+    style::TextStyleBuilder,
+};
 use rppal::{gpio, spi};
 use sh1106::{mode::GraphicsMode, Builder};
 use std::{thread, time};
 
 fn main() {
-    let mut spi0 = spi::Spi::new(
+    let spi0 = spi::Spi::new(
         spi::Bus::Spi0,
         spi::SlaveSelect::Ss0,
         400_000,
@@ -19,19 +23,31 @@ fn main() {
     )
     .unwrap();
     let spi_gpio = gpio::Gpio::new().unwrap();
-    let mut spi_dc = spi_gpio.get(24).unwrap().into_output();
-    let mut spi_cs = spi_gpio.get(8).unwrap().into_output();
+    let spi_dc = spi_gpio.get(24).unwrap().into_output();
+    let spi_cs = spi_gpio.get(8).unwrap().into_output();
     let mut screen_display: GraphicsMode<_> =
         Builder::new().connect_spi(spi0, spi_dc, spi_cs).into();
     screen_display.init().unwrap();
     screen_display.flush().unwrap();
-    screen_display.draw(Font6x8::render_str("Hello world!", 1u8.into()).into_iter());
-    thread::sleep(time::Duration::from_seconds(5));
-    screen_display.draw(
-        Font6x8::render_str("Hello Rust!")
-            .translate(Coord::new(0, 16))
-            .into_iter(),
-    );
-    thread::sleep(time::Duration::from_seconds(5));
+    let text_style = TextStyleBuilder::new(Font6x8).build();
+
+    Text::new("Hello Rust!", Point::new(20, 30))
+        .into_styled(text_style)
+        .draw(&mut screen_display)
+        .unwrap();
+
+    //    screen_display.draw(Font6x8::clone_into("Hello world!", 1u8.into()).into_iter());
+    thread::sleep(time::Duration::from_secs(5));
+    //    screen_display.draw(
+    //        Font6x8::render_str("Hello Rust!")
+    //            .translate(Coord::new(0, 16))
+    //            .into_iter(),
+    //    );
+    Text::new("Hello World!", Point::new(20, 30))
+        .into_styled(text_style)
+        .draw(&mut screen_display)
+        .unwrap();
+
+    thread::sleep(time::Duration::from_secs(5));
     screen_display.flush().unwrap();
 }
