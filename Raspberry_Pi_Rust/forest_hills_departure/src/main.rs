15c6704c37aa11e60cf52c00ed47afbf44c25133
Rory Coffey <coffeyrt@gmail.com>
2020-10-06 21:21:13 -0400

    added ssd1306 drivers

--- Raspberry_Pi_Rust/forest_hills_departure/src/main.rs
+++ Raspberry_Pi_Rust/forest_hills_departure/src/main.rs
@@ -3,16 +3,20 @@
 extern crate embedded_graphics;
 extern crate rppal; // Crate for SPI, I2C, and GPIO on raspberry pi
 extern crate sh1106; // Crate for current SPI oled display
+extern crate ssd1306;
 extern crate std;
 
 use embedded_graphics::prelude::*;
 use embedded_graphics::{
     fonts::{Font6x8, Text},
-    style::{TextStyleBuilder, TextStyle},
     pixelcolor::BinaryColor,
+    style::{TextStyle, TextStyleBuilder},
 };
 use rppal::{gpio, spi};
-use sh1106::{mode::GraphicsMode, Builder};
+use sh1106;
+use sh1106::mode::GraphicsMode;
+use ssd1306;
+use ssd1306::mode::GraphicsMode;
 use std::{thread, time};
 
 fn main() {
@@ -26,30 +30,37 @@ fn main() {
     let spi_gpio = gpio::Gpio::new().unwrap();
     let spi_dc = spi_gpio.get(24).unwrap().into_output();
     let spi_cs = spi_gpio.get(8).unwrap().into_output();
-    let mut screen_display: GraphicsMode<_> =
-        Builder::new().connect_spi(spi0, spi_dc, spi_cs).into();
-    screen_display.init().unwrap();
-    screen_display.flush().unwrap();
+    let mut screen_display_sh1106: GraphicsMode<_> = sh1106::Builder::new()
+        .connect_spi(spi0, spi_dc, spi_cs)
+        .into();
+    screen_display_sh1106.init().unwrap();
+    screen_display_sh1106.flush().unwrap();
     let text_style = TextStyleBuilder::new(Font6x8).build();
-    screen_display.set_pixel(10, 10, 1u8);
+    screen_display_sh1106.set_pixel(10, 10, 1u8);
 
-    Text::new("Hello Rust!", Point::new(20, 30))
+    Text::new("sh1106", Point::new(20, 30))
         .into_styled(TextStyle::new(Font6x8, BinaryColor::On))
-        .draw(&mut screen_display)
+        .draw(&mut screen_display_sh1106)
         .unwrap();
 
     //    screen_display.draw(Font6x8::clone_into("Hello world!", 1u8.into()).into_iter());
-    thread::sleep(time::Duration::from_secs(5));
-    //    screen_display.draw(
-    //        Font6x8::render_str("Hello Rust!")
-    //            .translate(Coord::new(0, 16))
-    //            .into_iter(),
-    //    );
-    Text::new("Hello World!", Point::new(20, 30))
-        .into_styled(text_style)
-        .draw(&mut screen_display)
+    thread::sleep(time::Duration::from_secs(1));
+    screen_display_sh1106.flush().unwrap();
+
+    let mut screen_display_ssd1306: GraphicsMode<_> = ssd1306::Builder::new()
+        .connect_spi(spi0, spi_dc, spi_cs)
+        .into();
+    screen_display_ssd1306.init().unwrap();
+    screen_display_ssd1306.flush().unwrap();
+    let text_style = TextStyleBuilder::new(Font6x8).build();
+    screen_display_ssd1306.set_pixel(10, 10, 1u8);
+
+    Text::new("ssd1306", Point::new(20, 30))
+        .into_styled(TextStyle::new(Font6x8, BinaryColor::On))
+        .draw(&mut screen_display_ssd1306)
         .unwrap();
 
-    thread::sleep(time::Duration::from_secs(5));
-    screen_display.flush().unwrap();
+    //    screen_display.draw(Font6x8::clone_into("Hello world!", 1u8.into()).into_iter());
+    thread::sleep(time::Duration::from_secs(1));
+    screen_display_ssd1306.flush().unwrap();
 }
