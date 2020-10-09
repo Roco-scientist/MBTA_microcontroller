fac6a37239a495113bfb6c4422de34cedc4385bb
Rory Coffey <coffeyrt@gmail.com>
2020-10-09 19:36:54 -0400

    changed the led number value and added a pause

--- Raspberry_Pi_Rust/clock_test/src/main.rs
+++ Raspberry_Pi_Rust/clock_test/src/main.rs
@@ -1,11 +1,19 @@
 extern crate ht16k33;
 extern crate rppal;
+extern crate std;
+
+use std::{collections::HashMap, thread, time};
 
 fn main() {
+    let number_conversion: HashMap<u8, u8> = [(0, 63), (1, 6), (2, 91), (3, 79), (4, 102)]
+        .iter()
+        .cloned()
+        .collect();
     let i2c = rppal::i2c::I2c::new().unwrap();
     //    let address = 70u8;
     let address = 112u8; // actually 0x70 in hexidecimal which goes to 112
     let mut clock = ht16k33::HT16K33::new(i2c, address);
-    let led_location = ht16k33::LedLocation::new(0, 0).unwrap();
+    let led_location = ht16k33::LedLocation::new(0, number_conversion.get(1).unwrap()).unwrap();
     clock.set_led(led_location, true).unwrap();
+    thread::sleep(time::Duration::from_secs(2));
 }
