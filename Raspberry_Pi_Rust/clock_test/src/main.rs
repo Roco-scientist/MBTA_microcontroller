68ca279fb627a19c9c62ea4cb9fc8ea23469683f
Rory Coffey <coffeyrt@gmail.com>
2020-10-09 19:06:32 -0400

    changed address

--- Raspberry_Pi_Rust/clock_test/src/main.rs
+++ Raspberry_Pi_Rust/clock_test/src/main.rs
@@ -1,10 +1,11 @@
-extern crate rppal;
 extern crate ht16k33;
+extern crate rppal;
 
 fn main() {
     let i2c = rppal::i2c::I2c::new().unwrap();
-    let address = 70u8;
+    //    let address = 70u8;
+    let address = 112u8; // actually 0x70 in hexidecimal which goes to 112
     let mut clock = ht16k33::HT16K33::new(i2c, address);
-    let led_location = ht16k33::LedLocation::new(0,0).unwrap();
+    let led_location = ht16k33::LedLocation::new(0, 0).unwrap();
     clock.set_led(led_location, true).unwrap();
 }
