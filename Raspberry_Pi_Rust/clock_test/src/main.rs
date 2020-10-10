7162a42a4675765920a6087a845506d245cca1bf
Rory Coffey <coffeyrt@gmail.com>
2020-10-09 20:48:56 -0400

    The clock display is finally working

--- Raspberry_Pi_Rust/clock_test/src/main.rs
+++ Raspberry_Pi_Rust/clock_test/src/main.rs
@@ -2,23 +2,20 @@ extern crate ht16k33;
 extern crate rppal;
 extern crate std;
 
-use std::{collections::HashMap, thread, time};
+use std::{thread, time};
 
 fn main() {
-    let number_conversion: HashMap<u8, u8> = [(0, 63), (1, 6), (2, 91), (3, 79), (4, 102)]
-        .iter()
-        .cloned()
-        .collect();
     let i2c = rppal::i2c::I2c::new().unwrap();
-    //    let address = 70u8;
     let address = 112u8; // actually 0x70 in hexidecimal which goes to 112
     let mut clock = ht16k33::HT16K33::new(i2c, address);
-    let led_location = ht16k33::LedLocation::new(0, *number_conversion.get(&1).unwrap()).unwrap();
     clock.initialize().unwrap();
     clock.set_display(ht16k33::Display::ON).unwrap();
     clock
         .set_dimming(ht16k33::Dimming::from_u8(7u8).unwrap())
         .unwrap();
+    let led_location = ht16k33::LedLocation::new(0, 0).unwrap();
     clock.set_led(led_location, true).unwrap();
     thread::sleep(time::Duration::from_secs(2));
+    clock.set_led(led_location, false).unwrap();
 }
+
