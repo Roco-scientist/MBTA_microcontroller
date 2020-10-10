5a5c6e0be1a32b26b4531a56c60ee30b8c059a6b
Rory Coffey <coffeyrt@gmail.com>
2020-10-09 21:40:24 -0400

    clock tests fully working now

--- Raspberry_Pi_Rust/clock_test/src/main.rs
+++ Raspberry_Pi_Rust/clock_test/src/main.rs
@@ -13,26 +13,26 @@ fn main() {
     clock
         .set_dimming(ht16k33::Dimming::from_u8(7u8).unwrap())
         .unwrap();
-    display_num(0u8, 6u8, clock, true);
+    display_num(0u8, 6u8, &mut clock, true);
     thread::sleep(time::Duration::from_secs(2));
-    display_num(0u8, 6u8, clock, false);
+    display_num(0u8, 6u8, &mut clock, false);
 }
 
-fn display_num(location: u8, number: u8, display: ht16k33::HT16K33, on: bool) -> () {
+fn display_num(location: u8, number: u8,display: &mut ht16k33::HT16K33<rppal::i2c::I2c>, on: bool) -> () {
     //   _   0
     //  |_|  5, 6, 1
     //  |_|  4, 3, 2
     let number_leds: HashMap<u8, Vec<u8>> = [
-        (0u8, [0u8, 1u8, 2u8, 3u8, 4u8, 5u8]),
-        (1u8, [1u8, 2u8]),
-        (2u8, [0u8, 1u8, 3u8, 4u8, 6u8]),
-        (3u8, [0u8, 1u8, 2u8, 3u8, 6u8]),
-        (4u8, [1u8, 2u8, 5u8, 6u8]),
-        (5u8, [0u8, 2u8, 3u8, 5u8, 6u8]),
-        (6u8, [0u8, 2u8, 3u8, 4u8, 5u8, 6u8]),
-        (7u8, [0u8, 1u8, 2u8]),
-        (8u8, [0u8, 1u8, 2u8, 3u8, 4u8, 5u8, 6u8]),
-        (9u8, [0u8, 1u8, 2u8, 3u8, 5u8, 6u8]),
+        (0u8, vec![0u8, 1u8, 2u8, 3u8, 4u8, 5u8]),
+        (1u8, vec![1u8, 2u8]),
+        (2u8, vec![0u8, 1u8, 3u8, 4u8, 6u8]),
+        (3u8, vec![0u8, 1u8, 2u8, 3u8, 6u8]),
+        (4u8, vec![1u8, 2u8, 5u8, 6u8]),
+        (5u8, vec![0u8, 2u8, 3u8, 5u8, 6u8]),
+        (6u8, vec![0u8, 2u8, 3u8, 4u8, 5u8, 6u8]),
+        (7u8, vec![0u8, 1u8, 2u8]),
+        (8u8, vec![0u8, 1u8, 2u8, 3u8, 4u8, 5u8, 6u8]),
+        (9u8, vec![0u8, 1u8, 2u8, 3u8, 5u8, 6u8]),
     ]
     .iter()
     .cloned()
@@ -40,7 +40,7 @@ fn display_num(location: u8, number: u8, display: ht16k33::HT16K33, on: bool) ->
 
     let leds = number_leds.get(&number).unwrap();
     for led in leds {
-        let led_location = ht16k33::LedLocation::new(location, led).unwrap();
+        let led_location = ht16k33::LedLocation::new(location, *led).unwrap();
         display.set_led(led_location, on).unwrap();
     }
 }
