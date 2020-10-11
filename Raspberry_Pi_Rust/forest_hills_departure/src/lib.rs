a633a626ea354eab23658c085243ed8064fe727b
Rory Coffey <coffeyrt@gmail.com>
2020-10-11 18:38:09 -0400

    moved clock numbers to struct and changed a lot in flow

--- Raspberry_Pi_Rust/forest_hills_departure/src/lib.rs
+++ Raspberry_Pi_Rust/forest_hills_departure/src/lib.rs
@@ -7,8 +7,35 @@ use chrono::prelude::*;
 use rppal::i2c::I2c;
 use std::{collections::HashMap, thread, time};
 
+/// Below are the led numbers for each number within the clock
+///   _   0
+///  |_|  5, 6, 1
+///  |_|  4, 3, 2
+///
+///  Create a hashmap below with each number and the corresponding leds that need to be
+///  turned on
+const NUMBER_LEDS: HashMap<u8, Vec<u8>> = [
+    (0u8, vec![0u8, 1u8, 2u8, 3u8, 4u8, 5u8]),
+    (1u8, vec![1u8, 2u8]),
+    (2u8, vec![0u8, 1u8, 3u8, 4u8, 6u8]),
+    (3u8, vec![0u8, 1u8, 2u8, 3u8, 6u8]),
+    (4u8, vec![1u8, 2u8, 5u8, 6u8]),
+    (5u8, vec![0u8, 2u8, 3u8, 5u8, 6u8]),
+    (6u8, vec![0u8, 2u8, 3u8, 4u8, 5u8, 6u8]),
+    (7u8, vec![0u8, 1u8, 2u8]),
+    (8u8, vec![0u8, 1u8, 2u8, 3u8, 4u8, 5u8, 6u8]),
+    (9u8, vec![0u8, 1u8, 2u8, 3u8, 5u8, 6u8]),
+]
+.iter()
+.cloned()
+.collect();
+
 pub struct ClockDisplay {
     display: ht16k33::HT16K33<I2c>,
+    minutes_ten: Option<u8>,
+    minutes_single: Option<u8>,
+    seconds_ten: Option<u8>,
+    seconds_single: Option<u8>,
 }
 
 impl ClockDisplay {
@@ -17,8 +44,9 @@ impl ClockDisplay {
         // create new i2c interface
         let i2c = I2c::new().unwrap();
         // connect to the I2C address found within raspberry pi.  This is converted to decimal
-        let address = 112u8; // actually 0x70 in hexidecimal which goes to 112
-                             // connect the ht16k33 clock chip to i2c connection on the address
+        // actually 0x70 in hexidecimal which goes to 112
+        let address = 112u8;
+        // connect the ht16k33 clock chip to i2c connection on the address
         let mut clock = ht16k33::HT16K33::new(i2c, address);
         clock.initialize().unwrap();
         // turn clock display on.  Would not work otherwise
@@ -28,7 +56,13 @@ impl ClockDisplay {
             .set_dimming(ht16k33::Dimming::from_u8(7u8).unwrap())
             .unwrap();
         // return ClockDisplay struct
-        ClockDisplay { display: clock }
+        ClockDisplay {
+            display: clock,
+            minutes_ten: None,
+            minutes_single: None,
+            seconds_ten: None,
+            seconds_single: None,
+        }
     }
 
     /// Dispalys the minutes:seconds until the next train on the clock display
@@ -51,11 +85,37 @@ impl ClockDisplay {
                 let second = minutes % 10u8;
                 let third = seconds / 10u8;
                 let fourth = seconds % 10u8;
-                self.display_num(0u8, first, true);
-                self.display_num(2u8, second, true);
-                self.display_colon(true);
-                self.display_num(6u8, third, true);
-                self.display_num(8u8, fourth, true);
+                if self.minutes_ten.is_none() {
+                    self.minutes_ten = first;
+                    self.minutes_single = second;
+                    self.seconds_ten = third;
+                    self.seconds_single = fourth;
+                    self.display_nums();
+                } else {
+                    if first != self.minutes_ten.unwrap() {
+                        change_number(0, first);
+                        self.minutes_ten = first;
+                    }
+                    if second != self.minutes_single.unwrap() {
+                        change_number(2, second);
+                        self.minutes_single = second;
+                    }
+                    if third != self.seconds_ten.unwrap() {
+                        change_number(6, third);
+                        self.seconds_ten = third;
+                    }
+                    if fourth != self.seconds_single.unwrap() {
+                        change_number(8, fourth);
+                        self.seconds_single = fourth;
+                    }
+                }
+            } else {
+                self.minutes_ten = None;
+                self.minutes_single = None;
+                self.seconds_ten = None;
+                self.seconds_single = None;
+                self.display.clear_display_buffer();
+                self.display.write_display_buffer().unwrap();
             }
             println!("{:?}:{:?}", minutes, seconds);
             println!("{:?}", diff);
@@ -63,39 +123,31 @@ impl ClockDisplay {
             self.display.clear_display_buffer();
             self.display.write_display_buffer().unwrap();
         }
+        self.display.clear_display_buffer();
+        self.display.write_display_buffer().unwrap();
+    }
+
+    /// Turns on all numbers
+    fn display_nums(&mut self) -> () {
+        // Retrieve a vec! of leds that need to be turned on for the numbers
+        // Then turn them on
+        let mut leds = NUMBER_LEDS.get(self.minutes_ten).unwrap();
+        turn_on_leds(leds, 0);
+        let mut leds = NUMBER_LEDS.get(self.minutes_single).unwrap();
+        turn_on_leds(leds, 2);
+        let mut leds = NUMBER_LEDS.get(self.seconds_ten).unwrap();
+        turn_on_leds(leds, 6);
+        let mut leds = NUMBER_LEDS.get(self.seconds_single).unwrap();
+        turn_on_leds(leds, 8);
+        self.display_colon(true);
     }
 
     /// Turns on the necessary leds for a number at the indicated location
-    fn display_num(&mut self, location: u8, number: u8, on: bool) -> () {
-        // Below are the led numbers for each number within the clock
-        //   _   0
-        //  |_|  5, 6, 1
-        //  |_|  4, 3, 2
-        //
-        //  Create a hashmap below with each number and the corresponding leds that need to be
-        //  turned on
-        let number_leds: HashMap<u8, Vec<u8>> = [
-            (0u8, vec![0u8, 1u8, 2u8, 3u8, 4u8, 5u8]),
-            (1u8, vec![1u8, 2u8]),
-            (2u8, vec![0u8, 1u8, 3u8, 4u8, 6u8]),
-            (3u8, vec![0u8, 1u8, 2u8, 3u8, 6u8]),
-            (4u8, vec![1u8, 2u8, 5u8, 6u8]),
-            (5u8, vec![0u8, 2u8, 3u8, 5u8, 6u8]),
-            (6u8, vec![0u8, 2u8, 3u8, 4u8, 5u8, 6u8]),
-            (7u8, vec![0u8, 1u8, 2u8]),
-            (8u8, vec![0u8, 1u8, 2u8, 3u8, 4u8, 5u8, 6u8]),
-            (9u8, vec![0u8, 1u8, 2u8, 3u8, 5u8, 6u8]),
-        ]
-        .iter()
-        .cloned()
-        .collect();
-
-        // Retrieve a vec! of leds that need to be turned on for the numbers
-        let leds = number_leds.get(&number).unwrap();
+    fn turn_on_leds(&mut self, leds: &Vec<u8>, location: u8) -> () {
         // Turn on/off each led
         for led in leds {
             let led_location = ht16k33::LedLocation::new(location, *led).unwrap();
-            self.display.set_led(led_location, on).unwrap();
+            self.display.set_led(led_location, true).unwrap();
         }
     }
 
@@ -105,6 +157,27 @@ impl ClockDisplay {
         for led in leds {
             let led_location = ht16k33::LedLocation::new(4u8, led).unwrap();
             self.display.set_led(led_location, on).unwrap();
+        }
+    }
+
+    fn change_number(&mut self, location: u8, new_number: u8) {
+        let old_number = match location {
+            0 => self.minutes_ten,
+            2 => self.minutes_single,
+            6 => self.seconds_ten,
+            8 => self.seconds_single,
+        };
+        let old_leds = NUMBER_LEDS.get(old_number).unwrap();
+        let new_leds = NUMBER_LEDS.get(new_number).unwrap();
+        let leds_off = old_leds.iter().filter(|led| !new_leds.contains(led));
+        let leds_on = new_leds.iter().filter(|led| !old_leds.contains(led));
+        for led in leds_off {
+            let led_location = ht16k33::LedLocation::new(location, led).unwrap();
+            self.display.set_led(led_location, false).unwrap();
+        }
+        for led in leds_on {
+            let led_location = ht16k33::LedLocation::new(location, led).unwrap();
+            self.display.set_led(led_location, true).unwrap();
         }
     }
 }
