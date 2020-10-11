57e2600c001fd063b914fb378a339a76ef6b4e48
Rory Coffey <coffeyrt@gmail.com>
2020-10-10 20:12:34 -0400

    moving display to a struct

--- Raspberry_Pi_Rust/forest_hills_departure/src/lib.rs
+++ Raspberry_Pi_Rust/forest_hills_departure/src/lib.rs
@@ -7,48 +7,60 @@ use chrono::prelude::*;
 use rppal::i2c::I2c;
 use std::{collections::HashMap, thread, time};
 
-pub fn initialize_display() -> Result<ht16k33::HT16K33<I2c>, Box<dyn std::error::Error>> {
-    let i2c = I2c::new()?;
-    let address = 112u8; // actually 0x70 in hexidecimal which goes to 112
-    let mut clock = ht16k33::HT16K33::new(i2c, address);
-    clock.initialize()?;
-    clock.set_display(ht16k33::Display::ON)?;
-    clock.set_dimming(ht16k33::Dimming::from_u8(7u8)?)?;
-    return Ok(clock);
+pub struct ClockDisplay {
+    display: ht16k33::HT16K33<I2c>,
 }
 
-pub fn display_time(train_time: chrono::DateTime<Utc>, display: &mut ht16k33::HT16K33<I2c>) -> () {
-    let now = chrono::Utc::now();
-    let diff = now.signed_duration_since(train_time);
-    println!("{:?}", diff);
-    display_num(0u8, 6u8, &mut display, true);
-    thread::sleep(time::Duration::from_secs(2));
-    display_num(0u8, 6u8, &mut display, false);
-}
+impl ClockDisplay {
+    /// Creates a new ClockDisplay struct
+    pub fn new() -> ClockDisplay {
+        // create new i2c interface
+        let i2c = I2c::new().unwrap();
+        let address = 112u8; // actually 0x70 in hexidecimal which goes to 112
+        let mut clock = ht16k33::HT16K33::new(i2c, address);
+        clock.initialize().unwrap();
+        clock.set_display(ht16k33::Display::ON).unwrap();
+        clock
+            .set_dimming(ht16k33::Dimming::from_u8(7u8).unwrap())
+            .unwrap();
+        ClockDisplay { display: clock }
+    }
 
-fn display_num(location: u8, number: u8, display: &mut ht16k33::HT16K33<I2c>, on: bool) -> () {
-    //   _   0
-    //  |_|  5, 6, 1
-    //  |_|  4, 3, 2
-    let number_leds: HashMap<u8, Vec<u8>> = [
-        (0u8, vec![0u8, 1u8, 2u8, 3u8, 4u8, 5u8]),
-        (1u8, vec![1u8, 2u8]),
-        (2u8, vec![0u8, 1u8, 3u8, 4u8, 6u8]),
-        (3u8, vec![0u8, 1u8, 2u8, 3u8, 6u8]),
-        (4u8, vec![1u8, 2u8, 5u8, 6u8]),
-        (5u8, vec![0u8, 2u8, 3u8, 5u8, 6u8]),
-        (6u8, vec![0u8, 2u8, 3u8, 4u8, 5u8, 6u8]),
-        (7u8, vec![0u8, 1u8, 2u8]),
-        (8u8, vec![0u8, 1u8, 2u8, 3u8, 4u8, 5u8, 6u8]),
-        (9u8, vec![0u8, 1u8, 2u8, 3u8, 5u8, 6u8]),
-    ]
-    .iter()
-    .cloned()
-    .collect();
+    /// Dispalys the minutes:seconds until the next train on the clock display
+    pub fn display_time_until(&self, train_time: chrono::DateTime<Utc>) -> () {
+        let now = chrono::Utc::now();
+        let diff = now.signed_duration_since(train_time);
+        println!("{:?}", diff);
+        display_num(0u8, 6u8, true);
+        thread::sleep(time::Duration::from_secs(2));
+        display_num(0u8, 6u8, false);
+    }
 
-    let leds = number_leds.get(&number).unwrap();
-    for led in leds {
-        let led_location = ht16k33::LedLocation::new(location, *led).unwrap();
-        display.set_led(led_location, on).unwrap();
+    /// Turns on the necessary leds for a number at the indicated location
+    fn display_num(&self, location: u8, number: u8, on: bool) -> () {
+        //   _   0
+        //  |_|  5, 6, 1
+        //  |_|  4, 3, 2
+        let number_leds: HashMap<u8, Vec<u8>> = [
+            (0u8, vec![0u8, 1u8, 2u8, 3u8, 4u8, 5u8]),
+            (1u8, vec![1u8, 2u8]),
+            (2u8, vec![0u8, 1u8, 3u8, 4u8, 6u8]),
+            (3u8, vec![0u8, 1u8, 2u8, 3u8, 6u8]),
+            (4u8, vec![1u8, 2u8, 5u8, 6u8]),
+            (5u8, vec![0u8, 2u8, 3u8, 5u8, 6u8]),
+            (6u8, vec![0u8, 2u8, 3u8, 4u8, 5u8, 6u8]),
+            (7u8, vec![0u8, 1u8, 2u8]),
+            (8u8, vec![0u8, 1u8, 2u8, 3u8, 4u8, 5u8, 6u8]),
+            (9u8, vec![0u8, 1u8, 2u8, 3u8, 5u8, 6u8]),
+        ]
+        .iter()
+        .cloned()
+        .collect();
+
+        let leds = number_leds.get(&number).unwrap();
+        for led in leds {
+            let led_location = ht16k33::LedLocation::new(location, *led).unwrap();
+            self.display.set_led(led_location, on).unwrap();
+        }
     }
 }
--- Raspberry_Pi_Rust/forest_hills_departure/src/main.rs
+++ Raspberry_Pi_Rust/forest_hills_departure/src/main.rs
@@ -4,7 +4,7 @@ extern crate chrono;
 use forest_hills_departure;
 
 fn main() {
-    let clock = forest_hills_departure::initialize_display().unwrap();
     let train_time = chrono::Utc::now().checked_add_signed(chrono::Duration::minutes(5));
-    forest_hills_departure::display_time(train_time, &mut clock);
+    let mut clock = forest_hills_departure::ClockDisplay::new();
+    clock.display_time_until(train_time)
 }
