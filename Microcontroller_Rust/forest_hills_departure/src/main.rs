195569494aed6c4125e9e56c347d947b640f0b1c
Rory Coffey <coffeyrt@gmail.com>
2020-10-18 21:41:11 -0400

    still working through multithreading

--- Microcontroller_Rust/forest_hills_departure/src/main.rs
+++ Microcontroller_Rust/forest_hills_departure/src/main.rs
@@ -10,104 +10,85 @@ extern crate cortex_m;
 extern crate cortex_m_rt;
 #[macro_use]
 extern crate rtic;
-extern crate heapless;
-extern crate stm32f3xx_hal;
-extern crate panic_semihosting;
 extern crate f3;
+extern crate heapless;
+extern crate panic_semihosting;
+extern crate stm32f3xx_hal;
 
-use chrono::{prelude::*, DateTime};
-use core::{cell::Cell, time::Duration};
+use chrono::{prelude::*, NaiveTime};
+use core::time::Duration;
 use forest_hills_departure;
 use heapless::{consts::*, Vec};
-use rtfm::app;
-use f3::hal::stm32f30x::delay;
+use panic_semihosting as _;
+use rtic::app;
+use rtic::{
+    app,
+    cyccnt::{Instant, U32Ext},
+};
 
-static TRAIN_TIMES: Option<Vec<chrono::NaiveTime, U10>>;
-static NOW: Resource<Cell<chrono::NaiveTime>, C1>;
-static mut DELAY: delay::Delay;
+const PERIOD_DISPLAY: u32 = 8_000;
+const PERIOD_TRAINS: u32 = 8_000_000;
 
-#[app(device = f3::hal::stm32f30x)]
+#[app(device = f3::hal::stm32f30x, monotonic = rtic::cyccnt::CYCCNT)]
 const APP: () = {
-    #[init(resources = [TRAIN_TIMES, DELAY, NOW])]
-    fn init() {
-        resources.NOW.lock(|now|{
-            *now = chrono::NaiveTime::from_hms(12, 27, 30);
-        });
+    struct Resources {
+        time_now: NaiveTime,
+        train_times: Option<Vec<chrono::NaiveTime, U10>>,
+    }
+    #[init(schedule = [get_trains, display_times])]
+    fn init(cx: init::Context) -> init::LateResources {
         let now_plus_5 = chrono::NaiveTime::from_hms(12, 32, 30);
         let mut train_vec: Vec<chrono::NaiveTime, U10> = Vec::new();
         train_vec.push(now_plus_5);
-        resources.TRAIN_TIMES.lock(|train_times|{
-            *train_times = Some(train_vec)
-        });
-        let cp = cortex_m::Peripherals::take().unwrap();
-        let dp = stm32f3xx_hal::Peripherals::take().unwrap();
-        let mut flash = dp.FLASH.constrain();
-        let mut rcc = dp.RCC.constrain();
-        let clocks = rcc.cfgr.freeze(&mut flash.acr);
-        resources.DELAY.lock(|delay|{
-            *delay = delay::Delay::new(cp.SYST, clocks);
-        });
+        init::LateResources {
+            time_now: NaiveTime::from_hms(12, 27, 30),
+            train_times: Some(train_vec),
+        };
+        cx.core.DCB.enable_trace();
+        cx.schedule
+            .display_times(cx.start + PERIOD_DISPLAY)
+            .unwrap();
+        cx.schedule.get_trains(cx.start + PERIOD_TRAINS).unwrap();
     }
 
-    #[interrupt(priority = 1, resources = [TRAIN_TIMES, NOW, DELAY])]
-    fn get_trains() {
-        loop {
-            resources.DELAY.delay_ms(60_000u16);
-            // let new_train_times = forest_hills_departure::train_time::train_times()
-            //     .unwrap_or_else(|err| panic!("{:?}", err));
-            resources.TRAIN_TIMES.lock(|train_times|{
-                if resources.NOW > train_times[0] {
-                    now_plus_5 = resources.NOW + Duration::from_secs(300u64);
-                    let mut train_vec: Vec<chrono::NaiveTime, U10> = Vec::new();
-                    train_vec.push(now_plus_5);
-                    *train_times = Some(train_vec);
-                }
-            });
-        }
+    #[task(schedule = [get_trains], resources = [time_now, train_times])]
+    fn get_trains(cx: get_trains::Context) {
+        cx.resources.train_times.lock(|train_times| {
+            if cx.resources.time_now > train_times.unwrap()[0] {
+                let now_plus_5 = cx.resources.time_now + chrono::Duration::minutes(5);
+                let mut train_vec: Vec<chrono::NaiveTime, U10> = Vec::new();
+                train_vec.push(now_plus_5);
+                *train_times = Some(train_vec);
+            }
+        });
+        cx.schedule
+            .get_trains(cx.scheduled + PERIOD_TRAINS)
+            .unwrap();
     }
 
-    #[interrupt(priority = 2, resources = [TRAIN_TIMES, NOW, DELAY])]
-    fn display_times() {
+    #[task(schedule = [display_times], priority = 2, resources = [time_now, train_times])]
+    fn display_times(cx: display_times::Context) {
         let minimum_display_min = 5i64;
         // create a new clock struct, this initializes the display
         let mut clock = forest_hills_departure::ht16k33_clock::ClockDisplay::new();
         // create a new screen struct, this initializes the display
         let mut screen = forest_hills_departure::ssd1306_screen::ScreenDisplay::new();
-        // clone the train_times to pass into thread
-        loop {
-            resources.DELAY.delay_ms(250u8);
-            *resources.NOW = NOW + Duration::from_ms(250u8)
-            // if there are some train times, display on clock and screen
-            if let Some(train_times) = resources.TRAIN_TIMES {
-                screen.display_trains(&train_times);
-                clock.display_time_until(&train_times, &minimum_display_min);
-            } else {
-                // if there are no train times, clear both displays
-                screen.clear_display(true);
-                clock.clear_display();
-            }
+        *cx.resources.time_now += chrono::Duration::milliseconds(250);
+        // if there are some train times, display on clock and screen
+        if let Some(train_times) = cx.resources.train_times {
+            screen.display_trains(&train_times);
+            clock.display_time_until(&train_times, &minimum_display_min);
+        } else {
+            // if there are no train times, clear both displays
+            screen.clear_display(true);
+            clock.clear_display();
         }
+        cx.schedule
+            .display_times(cx.scheduled + PERIOD_DISPLAY)
+            .unwrap();
     }
-
+    extern "C" {
+        fn SSI0();
+        fn QEI0();
+    }
 };
-// setup multitasking on microcontroller
-//tasks!(stm32f3xx_hal,
-//get_trains: Task{
-//    interrupt: Tim6,
-//    priority: P0,
-//    enabled: true
-//},
-//display_times: Task{
-//    interrupt: Time6,
-//    priority: P1,
-//    enabled: true
-//});
-
-
-//fn idle(priority: P0, threshold: T0) {
-//    loop {
-//        cortex_m_rtfm::wfi()
-//    }
-//}
-
-
