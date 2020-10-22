59639f803beb35318d93b93acc9a8ebd9bf38fb5
Rory Coffey <coffeyrt@gmail.com>
2020-10-22 07:55:18 -0400

    changed eprintln to panic

--- Raspberry_Pi_Rust/forest_hills_departure/src/main.rs
+++ Raspberry_Pi_Rust/forest_hills_departure/src/main.rs
@@ -14,21 +14,21 @@ fn main() {
     // between threads
     let train_times_option = Arc::new(Mutex::new(
         forest_hills_departure::train_time::train_times()
-            .unwrap_or_else(|err| eprintln!("ERROR - train_times - {}", err)),
+            .unwrap_or_else(|err| panic!("ERROR - train_times - {}", err)),
     ));
     // create a new clock struct, this initializes the display
     let mut clock = forest_hills_departure::ht16k33_clock::ClockDisplay::new(0x70)
-        .unwrap_or_else(|err| eprintln!("ERROR - ClockDisplay - {}", err));
+        .unwrap_or_else(|err| panic!("ERROR - ClockDisplay - {}", err));
     // create a new screen struct, this initializes the display
     let mut screen = forest_hills_departure::ssd1306_screen::ScreenDisplay::new(0x51)
-        .unwrap_or_else(|err| eprintln!("ERROR - ScreenDisplay - {}", err));
+        .unwrap_or_else(|err| panic!("ERROR - ScreenDisplay - {}", err));
     // clone the train_times to pass into thread
     let train_times_clone = Arc::clone(&train_times_option);
     // In a new thread find train times every minute and replace train_times with new value
     thread::spawn(move || loop {
         thread::sleep(time::Duration::from_secs(60));
         let new_train_times = forest_hills_departure::train_time::train_times()
-            .unwrap_or_else(|err| eprintln!("ERROR - train_times - {}", err));
+            .unwrap_or_else(|err| panic!("ERROR - train_times - {}", err));
         let mut old_train = train_times_clone.lock().unwrap();
         *old_train = new_train_times;
     });
@@ -41,18 +41,18 @@ fn main() {
         if let Some(train_times) = &*train_times_unlocked {
             screen
                 .display_trains(&train_times)
-                .unwrap_or_else(|err| eprintln!("ERROR - display_trains - {}", err));
+                .unwrap_or_else(|err| panic!("ERROR - display_trains - {}", err));
             clock
                 .display_time_until(&train_times, &minimum_display_min)
-                .unwrap_or_else(|err| eprintln!("ERROR - display_time_until - {}", err));
+                .unwrap_or_else(|err| panic!("ERROR - display_time_until - {}", err));
         } else {
             // if there are no train times, clear both displays
             screen
                 .clear_display(true)
-                .unwrap_or_else(|err| eprintln!("ERROR - clear_display - {}", err));
+                .unwrap_or_else(|err| panic!("ERROR - clear_display - {}", err));
             clock
                 .clear_display()
-                .unwrap_or_else(|err| eprintln!("ERROR - clear_display - {}", err));
+                .unwrap_or_else(|err| panic!("ERROR - clear_display - {}", err));
         }
     }
 }
